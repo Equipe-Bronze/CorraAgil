adbc1f8ee89da057774abaa5ce9dafa993f4c74e
jucaodamontanha <jucaodamontanha@gmail.com>
2024-06-17 18:40:23 -0300

    criado reposito e dependencias iniciais

---
+++ src/main/java/br/com/pipocaagil/CorraAgil/CorraAgilApplication.java
@@ -0,0 +1,13 @@
+package br.com.pipocaagil.CorraAgil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorraAgilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorraAgilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pipocaagil/CorraAgil/CorraAgilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pipocaagil.CorraAgil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorraAgilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
