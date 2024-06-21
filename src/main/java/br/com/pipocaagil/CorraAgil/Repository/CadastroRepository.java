4f5444e9b98fd8dcfbf4ca938b4a535151406a9e
jucaodamontanha <jucaodamontanha@gmail.com>
2024-06-21 13:45:57 -0300

    efetuado pequenas correcoes

--- src/main/java/br/com/pipocaagil/CorraAgil/Repository/CadastroRepository.java
+++ src/main/java/br/com/pipocaagil/CorraAgil/Repository/CadastroRepository.java
@@ -2,6 +2,8 @@ package br.com.pipocaagil.CorraAgil.Repository;
 
 import br.com.pipocaagil.CorraAgil.Model.CadastroModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CadastroRepository extends JpaRepository<CadastroModel,Long > {
 }
