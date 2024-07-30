6da7392ab236ab881838f4ba737f80730661c970
Neander Fontes <neanderffontes@gmail.com>
2024-07-30 20:25:00 +0100

    Implement Migration with flyway and mysql

---
+++ src/main/resources/db/migration/V2__Populate_Table_Corragil.sql
@@ -0,0 +1,4 @@
+INSERT INTO `corragil` (`id`, `email`, `nomecompleto`, `senha`) VALUES
+	(1, 'neanderr@hotmail.com', 'Neander Fontes Cerqueira Cavalcantti Barbosa', '12345678'),
+	(2, 'test2@hotmail.com', 'Teste Nome Completo 2', '12345678'),
+	(3, 'test3@hotmail.com', 'Teste Nome Completo 3 Update Test', '12345678');
