eefa37b96ff5d84858f4e3822245a02522743cbf
jucaodamontanha <jucaodamontanha@gmail.com>
2024-09-28 15:46:09 -0300

    criado arquivo para o deploy inicial

--- src/main/java/br/com/pipocaagil/CorraAgil/infra/security/SecurityConfig.java
+++ src/main/java/br/com/pipocaagil/CorraAgil/infra/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -66,4 +67,9 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+
+
 }
+
