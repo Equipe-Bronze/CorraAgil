dabb49009cd0b815750c0c99bc086afdba1bae4c
jucaodamontanha <jucaodamontanha@gmail.com>
2024-09-28 15:51:17 -0300

    feito correcoes de configuracoes excedentes

--- src/main/java/br/com/pipocaagil/CorraAgil/infra/security/SecurityConfig.java
+++ src/main/java/br/com/pipocaagil/CorraAgil/infra/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
